a16a627c254ea966c2ce0c6da15e2a0d0e716799
ikappaki <ikappaki@users.noreply.github.com>
2024-04-07 19:54:11 +0100

    basilisp kernel wip

--- echo_kernel/kernel.py
+++ echo_kernel/kernel.py
@@ -1,5 +1,32 @@
 from ipykernel.kernelbase import Kernel
 
+import basilisp
+from basilisp import main as basilisp
+from basilisp.lang import compiler as compiler
+from basilisp.lang import reader as reader
+from basilisp.lang import runtime as runtime
+from basilisp.lang import symbol as sym
+from typing import Any, Callable, Optional, Sequence, Type
+import traceback
+
+from io import StringIO
+
+def eval_str(s: str, ctx: compiler.CompilerContext, ns: runtime.Namespace, eof: Any):
+    """Evaluate the forms in a string into a Python module AST node."""
+    last = eof
+    for form in reader.read_str(s, resolver=runtime.resolve_alias, eof=eof):
+        assert not isinstance(form, reader.ReaderConditional)
+        last = compiler.compile_and_exec_form(form, ctx, ns)
+    return last
+
+print(":hi")
+
+opts = {}
+basilisp.init(opts)
+ctx = compiler.CompilerContext(filename="basilisp-kernel", opts=opts)
+eof = object()
+ns = runtime.Namespace.get_or_create(runtime.CORE_NS_SYM)
+
 class EchoKernel(Kernel):
     implementation = 'Echo'
     implementation_version = '1.0'
@@ -14,11 +41,37 @@ class EchoKernel(Kernel):
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
-        if not silent:
-            stream_content = {'name': 'stdout', 'text': code}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+        bas_out = StringIO()
+        bas_err = StringIO()
+        status = "ok"
+        try:
+            with runtime.bindings({
+                    runtime.Var.find_safe(sym.symbol("*out*", ns=runtime.CORE_NS)) : bas_out,
+                    runtime.Var.find_safe(sym.symbol("*err*", ns=runtime.CORE_NS)) : bas_err
+            }):
+                result = eval_str(code, ctx, ns, eof)
+                if not silent:
+                    out = f"{bas_out.getvalue()}\n{str(result)}" if bas_out.tell() > 0 else str(result)
+                    stream_content = {'name': 'stdout', 'text': out}
+                    self.send_response(self.iopub_socket, 'stream', stream_content)
+                serr = bas_err.getvalue() 
+                if bas_err.tell() > 0:
+                    stream_content = {'name': 'stderr', 'text': bas_err.getvalue()}
+                    self.send_response(self.iopub_socket, 'stream', stream_content)
 
-        return {'status': 'ok',
+        # except reader.SyntaxError as e:
+        #     exc = format_exception(e, reader.SyntaxError, e.__traceback__)
+        # except compiler.CompilerException as e:
+        #     exc = format_exception(e, compiler.CompilerException, e.__traceback__)
+        # except Exception as e:  # pylint: disable=broad-exception-caught
+        #     exc = format_exception(e, Exception, e.__traceback__)
+
+        except Exception as e:
+            stream_content = {'name': 'stderr', 'text': traceback.format_exc()}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+            status = "error"
+
+        return {'status': status,
                 # The base class increments the execution count
                 'execution_count': self.execution_count,
                 'payload': [],
